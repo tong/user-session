2fb781b2fcb10b175484fdc84b0d31e95e9e8413
tong <tong@disktree.net>
2024-04-23 12:13:52 +0200

    ...

--- main.go
+++ main.go
@@ -9,16 +9,19 @@ import (
 	"github.com/google/uuid"
 )
 
+// const sessionExpireTime = 120 * time.Second
+const sessionExpireTime = 120 * time.Minute
+
 var users = map[string]string{
 	"tong":   "test",
 	"kilmou": "test",
 }
 
-var sessions = map[string]session{}
+var sessions = map[string]Session{}
 
-type session struct {
-	username string
-	expiry   time.Time
+type Session struct {
+	Username string    `json:"username"`
+	Expiry   time.Time `json:"expiry"`
 }
 
 type Credentials struct {
@@ -26,16 +29,14 @@ type Credentials struct {
 	Username string `json:"username"`
 }
 
-func (s session) isExprired() bool {
-	return s.expiry.Before(time.Now())
+func (s Session) isExprired() bool {
+	return s.Expiry.Before(time.Now())
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("signin...")
 	var creds Credentials
 	err := json.NewDecoder(r.Body).Decode(&creds)
-
-	fmt.Println(err)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
@@ -47,11 +48,12 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	sessionToken := uuid.NewString()
-	expiresAt := time.Now().Add(120 * time.Second)
-	sessions[sessionToken] = session{
-		username: creds.Username,
-		expiry:   expiresAt,
-	}
+	expiresAt := time.Now().Add(sessionExpireTime)
+	// sessions[sessionToken] = session{
+	// 	username: creds.Username,
+	// 	expiry:   expiresAt,
+	// }
+	sessions[sessionToken] = Session{creds.Username, expiresAt}
 	http.SetCookie(w, &http.Cookie{
 		Name:    "session_token",
 		Value:   sessionToken,
@@ -98,21 +100,22 @@ func Refresh(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	newSessionToken := uuid.NewString()
-	expiresAt := time.Now().Add(120 * time.Second)
-	sessions[newSessionToken] = session{
-		username: userSession.username,
-		expiry:   expiresAt,
-	}
+	expiresAt := time.Now().Add(sessionExpireTime)
+	// sessions[newSessionToken] = session{
+	// 	username: userSession.username,
+	// 	expiry:   expiresAt,
+	// }
+	sessions[newSessionToken] = Session{userSession.Username, expiresAt}
 	delete(sessions, sessionToken)
 	http.SetCookie(w, &http.Cookie{
 		Name:    "session_token",
 		Value:   newSessionToken,
-		Expires: time.Now().Add(120 * time.Second),
+		Expires: time.Now().Add(sessionExpireTime),
 	})
 }
 
-func Welcome(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("welcome...")
+func Status(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("status...")
 	c, err := r.Cookie("session_token")
 	if err != nil {
 		if err == http.ErrNoCookie {
@@ -133,20 +136,72 @@ func Welcome(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusUnauthorized)
 		return
 	}
-	w.Write([]byte(fmt.Sprintf("Welcome %s!", userSession.username)))
+	// w.Write([]byte(fmt.Sprintf("user: %s\nexpires: %s", userSession.username, userSession.expiry)))
+	// w.Write([]byte(fmt.Sprintf("{\"user\": \"%s\", \"expires\": \"%s\"}", userSession.username, userSession.expiry)))
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(userSession)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("home...")
-	fmt.Fprintf(w, "Home\n")
+func Register(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("regster...TODO")
 }
+
+func Unregister(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("unregster...TODO")
+}
+
+/*
+func Test(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("test...", r.Body)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	// data := TestData{Username: "king"}
+	data := TestData{Username: "king"}
+	// data := `{"username": "king"}`
+	fmt.Println(data)
+	// fmt.Println(data.username)
+	err := json.NewEncoder(w).Encode(data)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	// w.WriteHeader(http.StatusOK)
+}
+
+type TestData struct {
+	// Username string `json:"username"`
+	Username string
+}
+*/
 
 func main() {
+	/*
+		// data := `{"username": "king"}`
+		data := TestData{"king"}
+		// err := json.Unmarshal([]byte(data), &obj)
+		b, err := json.Marshal(data)
+		fmt.Println(err)
+		fmt.Println(b)
+
+		var obj TestData
+		err = json.Unmarshal(b, &obj)
+		fmt.Println(err)
+		fmt.Println(obj)
+	*/
 	// http.HandleFunc("/headers", headers)
 	// http.HandleFunc("/", Home)
 	http.HandleFunc("/auth/login", Login)
 	http.HandleFunc("/auth/logout", Logout)
 	http.HandleFunc("/auth/refresh", Refresh)
-	http.HandleFunc("/auth/welcome", Welcome)
-	http.ListenAndServe(":8090", nil)
+	http.HandleFunc("/auth/status", Status)
+	http.HandleFunc("/auth/register", Register)
+	http.HandleFunc("/auth/unregister", Unregister)
+	// http.HandleFunc("/auth/test", Test)
+	http.ListenAndServe(":16000", nil)
 }
